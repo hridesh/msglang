8bbc60a29990266cc679e9510c6fb915908090cf
Hridesh Rajan <hridesh@iastate.edu>
2020-06-12 14:08:32 -0500

    small fix in prompt

--- src/msglang/Interpreter.java
+++ src/msglang/Interpreter.java
@@ -14,12 +14,8 @@ import msglang.AST.*;
  */
 public class Interpreter {
 	public static void main(String[] args) {
-		System.out.println("ActLang: Type a program to evaluate and press the enter key,\n" + 
-				"e.g. (ref 342) \n" + 
-				"or try (deref (ref 342)) \n" +
-				"or try (let ((class (ref 342))) (deref class)) \n" +
-				"or try (let ((class (ref 342))) (set! class 541)) \n" + 
-				"or try  (let ((r (ref 342))) (let ((d (free r))) (deref r))) \n" +
+		System.out.println("MsgLang: Type a program to evaluate and press the enter key,\n" + 
+				"e.g. (send (process (num) (print num)) 342) \n" + 
 				"Press Ctrl + C to exit.");
 		Reader reader = new Reader();
 		Evaluator eval = new Evaluator(reader);
