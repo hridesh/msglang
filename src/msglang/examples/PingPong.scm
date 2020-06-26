75c29b123eb8084acec843262f9c51f6d51f5aa5
Hridesh Rajan <hridesh@iastate.edu>
2020-06-25 21:17:12 -0500

    fix ping pong example

--- src/msglang/examples/PingPong.scm
+++ src/msglang/examples/PingPong.scm
@@ -1,27 +1,31 @@
 (define seq (lambda (cmd1 cmd2) cmd1))
 
 (define ping 
-	(process (sender num)
-		(if
-			(> num 0) 
-			(seq 
-				(print "ping") 
-				(send sender (self) (- num 1))
-			)  
-			(stop)
+	(process 
+		(receive (sender num)
+			(if
+				(> num 0) 
+				(seq 
+					(print "ping") 
+					(send sender (self) (- num 1))
+				)  
+				(stop)
+			)
 		)
 	)
 )
 
 (define pong 
-	(process (sender num)
-		(if
-			(> num 0) 
-			(seq 
-				(print "pong") 
-				(send sender (self) (- num 1))
+	(process 
+		(receive (sender num)
+			(if
+				(> num 0) 
+				(seq 
+					(print "pong") 
+					(send sender (self) (- num 1))
+				)
+				(stop)
 			)
-			(stop)
 		)
 	)
 ) 
