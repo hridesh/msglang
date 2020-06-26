9bcddd2070e6e41dbab96f2ffd9d7f5d5eb8a3ac
Hridesh Rajan <hridesh@iastate.edu>
2020-06-25 21:16:52 -0500

    fix request response example

--- src/msglang/examples/RequestResponse.scm
+++ src/msglang/examples/RequestResponse.scm
@@ -1,14 +1,18 @@
 (define worker 
-	(process (num1 num2)
-		(send client #f (+ num1 num2) 0)
+	(process 
+		(receive (client num1 num2)
+			(send client #f (+ num1 num2) 0)
+		)
 	)
 )
 
 (define client
-	(process (request num1 num2)
-		(if request
-			(send worker num1 num2)
-			(print num1)
+	(process 
+		(receive (request num1 num2)
+			(if request
+				(send worker (self) num1 num2)
+				(print num1)
+			)
 		)
 	)
 )
